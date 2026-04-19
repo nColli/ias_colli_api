311aaf53e65948efffdadbeb97726b263e84f959
nColli <collinicolas01@gmail.com>
2026-04-19 11:01:34 -0300

    Test boilerplate 2

--- tests/security/test_security.py
+++ tests/security/test_security.py
@@ -1,12 +1,2 @@
-import pytest
-from app import app
-
-@pytest.fixture
-def client():
-    app.config["TESTING"] = True
-    with app.test_client() as client:
-        yield client
-
-def test_method_not_allowed(client):
-    res = client.post("/")
-    assert res.status_code == 405
+def test_placeholder():
+    assert True
--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -1,12 +1,2 @@
-import pytest
-from app import app
-
-@pytest.fixture
-def client():
-    app.config["TESTING"] = True
-    with app.test_client() as client:
-        yield client
-
-def test_hello_world(client):
-    res = client.get("/")
-    assert res.status_code == 200
+def test_placeholder():
+    assert True
