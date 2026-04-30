9176c15aa4ee38a0c8e493a7444a68fc9d1c630d
Nicolas Colli <collinicolas01@gmail.com>
2026-04-30 17:37:52 -0300

    Dashboard

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Api
 from .routes import register_routes
 
@@ -7,4 +7,9 @@ def create_app():
     app = Flask(__name__)
     api = Api(app)
     register_routes(api)
+
+    @app.route("/")
+    def dashboard():
+        return render_template("dashboard.html")
+
     return app
