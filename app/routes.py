a73bd339048880a6e284741127617b35e1c1ee34
Nicolas Colli <collinicolas01@gmail.com>
2026-05-14 21:10:13 -0300

    fix simulacion error

--- app/routes.py
+++ app/routes.py
@@ -39,7 +39,6 @@ class UserList(Resource):
         conn = get_connection()
         cur = conn.cursor()
         cur.execute(
-            # "INSERT INTO users (nombre, apellido, rol) VALUES (%s, %s, %s) RETURNING id",
             "INSERT INTO users (nombre, apellido, puesto) VALUES (%s, %s, %s) RETURNING id",
             (data["nombre"], data["apellido"], data["rol"]),
         )
